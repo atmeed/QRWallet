0f134c79004eecdac48fa66abb0096ce1e4fd93b
atmeed <leo.zemtsoff@gmail.com>
2022-01-25 21:30:09 +0300

    Initial Commit

---
+++ QRWallet/QRWalletApp.swift
@@ -0,0 +1,17 @@
+//
+//  QRWalletApp.swift
+//  QRWallet
+//
+//  Created by Leonid Zemtsov on 25.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct QRWalletApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
